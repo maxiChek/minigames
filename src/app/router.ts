25afcbad73b9797837598078605ccf188b8fc2c9
Maksim Grekov <mgrekov5@gmail.com>
2026-09-21 01:07:40 +0300

    feat: implement leaderboard section

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,6 +2,7 @@ import { createHeader } from '../components/header/header';
 import '../components/header/header.scss';
 import { createHomePage } from '../pages/home/home-page';
 import '../pages/home/home-page.scss';
+import '../components/leaderboard/leaderboard.scss';
 
 export function initRouter(): void {
   const app = document.querySelector<HTMLDivElement>('#app')!;
